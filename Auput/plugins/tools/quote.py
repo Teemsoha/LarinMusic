a1ce55fb9aaf9975c695d534dc3f5db5b1d31e81
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-16 21:06:16 +0300

    Update quote.py

--- Auput/plugins/tools/quote.py
+++ Auput/plugins/tools/quote.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from typing import Union
-from YukkiMusic import app
+from Auput import app
 
 @app.on_message(filters.command("طرح"))
 async def Saidi(_, message: Message):
